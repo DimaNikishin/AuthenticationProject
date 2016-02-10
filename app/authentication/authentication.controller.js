3ba6ec93d0ac46045be1fbb2b4a06a39b39cd4e8
DimaNikishin <dimanikishin@yandex.ru>
2016-02-10 10:32:36 +0200

    auth service

--- app/authentication/authentication.controller.js
+++ app/authentication/authentication.controller.js
@@ -1,3 +1,22 @@
-/**
- * Created by dimanikishin on 09.02.16.
- */
+(function(){
+  'use strict';
+
+  /**
+   * @memberof myApp.authentication
+   * @ngdoc controller
+   * @name authenticationController
+   */
+
+  angular
+    .module('myApp.authentication')
+    .controller('authenticationController',authenticationController);
+
+  authenticationController.$inject = ['$scope', 'authenticationService'];
+
+  function authenticationController($scope, authenticationService){
+    var that = this;
+    that.Login = function(data){
+      authenticationService.login(angular.toJson(data))
+    }
+  }
+})();
--- app/authentication/authentication.factory.js
+++ app/authentication/authentication.factory.js
@@ -1,3 +1,30 @@
-/**
- * Created by dimanikishin on 09.02.16.
- */
+(function(){
+  'use strict';
+
+  /**
+   * @memberof myApp.authentication
+   * @ngdoc factory
+   * @name authenticationService
+   */
+
+  angular
+    .module('myApp.authentication')
+    .factory('authenticationService',authenticationService);
+
+  authenticationService.$inject = ['$http'];
+
+  function authenticationService($http){
+    var service = {
+      login : login
+    }
+
+    return service;
+
+    function login(data){
+      $http.post("https://93.183.203.13:10443/login", data).success(function(data, status) {
+        console.log(data);
+        console.log(111);
+      })
+    }
+  }
+})();
